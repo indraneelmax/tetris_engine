2deb9a5b13c1b81e3c12b1bcab3bdcb385f07210
Indraneel Srivastava <indraneel.max@gmail.com>
2022-07-10 13:52:16 +0100

    implement get_occupied_cells for all shapes

--- tetris_engine/shapes.py
+++ tetris_engine/shapes.py
@@ -28,7 +28,7 @@ class InShape(object):
         self.left_col = left_col
         pass
 
-    def get_occupied_cells(pos_cord):
+    def get_occupied_cells(self, pos_cord):
         """
         Retruns a list of coordinates that are occupied by
         the shape based on the input coordinate representing
@@ -54,24 +54,23 @@ class QShape(InShape):
     def __init__(self, left_col=0):
         super(QShape, self).__init__(left_col)
 
-
-def get_occupied_cells(pos_cord):
-    """
-        Args:
-            pos_cord (Coordinate): Top left coordinate representing
-              the position of the shape.
-        Returns:
-            list(Coordinate): List of Coordinates occupied by the
-              shape.
+    def get_occupied_cells(self, pos_cord):
         """
-    row = pos_cord.row
-    col = pos_cord.col
-    return [
-        Coordinate(row, col),
-        Coordinate(row, col + 1),
-        Coordinate(row - 1, col),
-        Coordinate(row - 1, col - 1),
-    ]
+            Args:
+                pos_cord (Coordinate): Top left coordinate representing
+                the position of the shape.
+            Returns:
+                list(Coordinate): List of Coordinates occupied by the
+                shape.
+            """
+        row = pos_cord.row
+        col = pos_cord.col
+        return [
+            Coordinate(row, col),
+            Coordinate(row, col + 1),
+            Coordinate(row - 1, col),
+            Coordinate(row - 1, col - 1),
+        ]
 
 
 class ZShape(InShape):
@@ -81,6 +80,24 @@ class ZShape(InShape):
     def __init__(self, left_col=0):
         super(ZShape, self).__init__(left_col)
 
+    def get_occupied_cells(self, pos_cord):
+        """
+            Args:
+                pos_cord (Coordinate): Top left coordinate representing
+                the position of the shape.
+            Returns:
+                list(Coordinate): List of Coordinates occupied by the
+                shape.
+            """
+        row = pos_cord.row
+        col = pos_cord.col
+        return [
+            Coordinate(row, col),
+            Coordinate(row, col + 1),
+            Coordinate(row - 1, col + 1),
+            Coordinate(row - 1, col + 2),
+        ]
+
 
 class SShape(InShape):
 
@@ -88,6 +105,24 @@ class SShape(InShape):
 
     def __init__(self, left_col=0):
         super(SShape, self).__init__(left_col)
+
+    def get_occupied_cells(self, pos_cord):
+        """
+            Args:
+                pos_cord (Coordinate): Top left coordinate representing
+                the position of the shape.
+            Returns:
+                list(Coordinate): List of Coordinates occupied by the
+                shape.
+            """
+        row = pos_cord.row
+        col = pos_cord.col
+        return [
+            Coordinate(row, col + 1),
+            Coordinate(row, col + 2),
+            Coordinate(row - 1, col),
+            Coordinate(row - 1, col + 1),
+        ]
 
 
 class TShape(InShape):
@@ -97,6 +132,24 @@ class TShape(InShape):
     def __init__(self, left_col=0):
         super(TShape, self).__init__(left_col)
 
+    def get_occupied_cells(self, pos_cord):
+        """
+            Args:
+                pos_cord (Coordinate): Top left coordinate representing
+                the position of the shape.
+            Returns:
+                list(Coordinate): List of Coordinates occupied by the
+                shape.
+            """
+        row = pos_cord.row
+        col = pos_cord.col
+        return [
+            Coordinate(row, col),
+            Coordinate(row, col + 1),
+            Coordinate(row, col + 2),
+            Coordinate(row - 1, col + 1),
+        ]
+
 
 class IShape(InShape):
 
@@ -104,6 +157,24 @@ class IShape(InShape):
 
     def __init__(self, left_col=0):
         super(IShape, self).__init__(left_col)
+
+    def get_occupied_cells(self, pos_cord):
+        """
+            Args:
+                pos_cord (Coordinate): Top left coordinate representing
+                the position of the shape.
+            Returns:
+                list(Coordinate): List of Coordinates occupied by the
+                shape.
+            """
+        row = pos_cord.row
+        col = pos_cord.col
+        return [
+            Coordinate(row, col),
+            Coordinate(row, col + 1),
+            Coordinate(row, col + 2),
+            Coordinate(row, col + 3),
+        ]
 
 
 class LShape(InShape):
@@ -113,6 +184,24 @@ class LShape(InShape):
     def __init__(self, left_col=0):
         super(LShape, self).__init__(left_col)
 
+    def get_occupied_cells(self, pos_cord):
+        """
+            Args:
+                pos_cord (Coordinate): Top left coordinate representing
+                the position of the shape.
+            Returns:
+                list(Coordinate): List of Coordinates occupied by the
+                shape.
+            """
+        row = pos_cord.row
+        col = pos_cord.col
+        return [
+            Coordinate(row, col),
+            Coordinate(row - 1, col),
+            Coordinate(row - 2, col),
+            Coordinate(row - 2, col + 1),
+        ]
+
 
 class JShape(InShape):
 
@@ -120,3 +209,21 @@ class JShape(InShape):
 
     def __init__(self, left_col=0):
         super(JShape, self).__init__(left_col)
+
+    def get_occupied_cells(self, pos_cord):
+        """
+            Args:
+                pos_cord (Coordinate): Top left coordinate representing
+                the position of the shape.
+            Returns:
+                list(Coordinate): List of Coordinates occupied by the
+                shape.
+            """
+        row = pos_cord.row
+        col = pos_cord.col
+        return [
+            Coordinate(row, col + 1),
+            Coordinate(row - 1, col + 1),
+            Coordinate(row - 2, col + 1),
+            Coordinate(row - 2, col),
+        ]
